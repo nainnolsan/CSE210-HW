67315ec579534cf9bb016311865e836c13a94537
nainnolsan <nain.nolasco.san@gmail.com>
2025-09-20 23:28:47 -0600

    Done!

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Crear instancias de trabajos
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
@@ -17,13 +16,11 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        // Crear instancia de Resume y agregar trabajos
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
-        // Mostrar todo el resume usando el método Display
         myResume.Display();
     }
 }

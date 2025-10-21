b5d13bdb7f6b9274049172ea084877b0f3f7d422
nainnolsan <nain.nolasco.san@gmail.com>
2025-10-21 12:58:39 -0600

    develop 03 doen

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding.";
+        Scripture scripture = new Scripture(reference, text);
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+
+        while (true)
+        {
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to end.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(3);
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("\nAll words are hidden! Good job memorizing!");
+                break;
+            }
+        }
+
+        // Creativity & Extra Credit:
+        // I added a feature that gives a random motivational message
+        // after all words are hidden to encourage memorization progress.
+        string[] messages = {
+            "You did it! Keep the word in your heart!",
+            "Great job! Another verse memorized!",
+            "Awesome! You’re becoming a scripture master!"
+        };
+
+        if (scripture.IsCompletelyHidden())
+        {
+            Random rnd = new Random();
+            Console.WriteLine(messages[rnd.Next(messages.Length)]);
+        }
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(' ');
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random random = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} - {text}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}

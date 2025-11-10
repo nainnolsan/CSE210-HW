208c7738209ae23ecfbe30464fe6a843e83623ec
nainnolsan <nain.nolasco.san@gmail.com>
2025-11-10 16:16:15 -0700

    Done!

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea():0.00}");
+        }
     }
 }
